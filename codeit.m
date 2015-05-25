33be2c4934297b9ee8fa3ddce2f8b3a74c3c3137
Amr <amr.e.dev@gmail.com>
2015-05-25 19:38:37 +0300

    not all reversed

--- codeit.m
+++ codeit.m
@@ -7,8 +7,8 @@
 % It replaces each 'a' with 'z', each 'b' with 'y', each 'c' with 'x',
 
 function coded = codeit (txt)
-    % upperalpha = cellstr(['A'; 'B'; 'C'; 'D'; 'E'; 'F'; 'G'; 'H'; 'I'; 'J'; 'K'; 'L'; 'M'; 'N'; 'O'; 'P'; 'Q'; 'R'; 'S'; 'T'; 'U'; 'V'; 'W'; 'X'; 'Y'; 'Z']);
-    % loweralpha = cellstr(['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z']);
+    % 
+    % 
     % outputchar = char(inputchar + 25);
     % 65 - 90 A-Z
     % 97 - 122 a-z
@@ -19,10 +19,16 @@ function coded = codeit (txt)
         if (txtNum(i) >= 65 & txtNum(i) <= 90)
             %fprintf('upper case\n');
             % print output
-            coded(end+1) = txtNum(i);
+            upperalpha = repmat(cellstr(['A'; 'B'; 'C'; 'D'; 'E'; 'F'; 'G'; 'H'; 'I'; 'J'; 'K'; 'L'; 'M'; 'N'; 'O'; 'P'; 'Q'; 'R'; 'S'; 'T'; 'U'; 'V'; 'W'; 'X'; 'Y'; 'Z']), 5, 1);
+            index = txtNum(i)-64;
+            coded(end+1) = double(char(upperalpha(i+25)));
+            index = 0;
         elseif (txtNum(i) >= 97 & txtNum(i) <= 122)
             %fprintf('lower case\n');
-            coded(end+1) = txtNum(i);
+            loweralpha = repmat(cellstr(['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z']), 5, 1);
+            index = txtNum(i)-96;
+            coded(end+1) = double(char(loweralpha(i+25)));
+            index = 0;
         end
     end
     coded = char(coded);
