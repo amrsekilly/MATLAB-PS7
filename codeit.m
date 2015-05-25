c31bd4ec9e0b2cef457dd120c88529be92d0b583
Amr <amr.e.dev@gmail.com>
2015-05-25 19:28:00 +0300

    echo done

--- codeit.m
+++ codeit.m
@@ -10,4 +10,20 @@ function coded = codeit (txt)
     % upperalpha = cellstr(['A'; 'B'; 'C'; 'D'; 'E'; 'F'; 'G'; 'H'; 'I'; 'J'; 'K'; 'L'; 'M'; 'N'; 'O'; 'P'; 'Q'; 'R'; 'S'; 'T'; 'U'; 'V'; 'W'; 'X'; 'Y'; 'Z']);
     % loweralpha = cellstr(['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z']);
     % outputchar = char(inputchar + 25);
+    % 65 - 90 A-Z
+    % 97 - 122 a-z
+    % string to num: cast with double('string')
+    txtNum = double(txt);
+    coded = [];
+    for i = 1 : length(txtNum)
+        if (txtNum(i) >= 65 & txtNum(i) <= 90)
+            %fprintf('upper case\n');
+            % print output
+            coded(end+1) = txtNum(i);
+        elseif (txtNum(i) >= 97 & txtNum(i) <= 122)
+            %fprintf('lower case\n');
+            coded(end+1) = txtNum(i);
+        end
+    end
+    coded = char(coded);
 end
