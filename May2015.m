03c740e4281cd4c1714e867983f61f285ad7c192
Amr <amr.e.dev@gmail.com>
2015-05-25 11:51:47 +0300

    init 2

---
+++ May2015.m
@@ -0,0 +1,12 @@
+% Introduction to Programming with MATLAB - MOOC
+% Homework 7 - Problem 2
+
+% inputs:
+% outputs: daysofmay - struct - fields: ?month?, ?date?, and ?day?
+% The month field must contain the string 'May' (uppercase ?M?).
+% The date field must contain a scalar of type double that equals the date (1 through 31).
+% The day field must contain the three-letter abbreviation of the day chosen from this list: 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'.
+
+function daysofmay = May2015
+    
+end
