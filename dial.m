e793dbcceaffbfd887bebadb6637142dde244b13
Amr <amr.e.dev@gmail.com>
2015-05-26 16:58:55 +0300

    echo spec

--- dial.m
+++ dial.m
@@ -4,12 +4,22 @@
 % Input: strnum :  '1 (FUN) DOG-4-YOU #2' 
 % Output: num  :  '1 386 364 4 968 #2'
 function num = dial (strnum)
+    % num stores all the number returned "whole number with everything"
     num = [];
+    % convert the input as a whole to numbers
     texttonums = double(strnum);
     for i = 1 : length(texttonums)
         % check for uppercase alphabet
         if (texttonums(i) >= 65 & texttonums(i) <= 90)
             num(end+1) = chartonum (texttonums(i));
+            
+            % check for special characters
+            % it does not change the digits, the # and * signs, or the spaces
+            % it replaces each parenthesis with a space and each dash with a space
+            % # is 35 , * is 42 , space is 32
+        elseif (texttonums(i) == 35 || texttonums(i) == 42 || texttonums(i) == 32)
+            % echo back the same char to the num array
+            num = strcat(num2str(num), char(texttonums(i)));
         end
     end
 end
