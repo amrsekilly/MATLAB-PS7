614d02b021c2403292d6964cefe8ddaa19843dee
Amr <amr.e.dev@gmail.com>
2015-05-28 12:05:48 +0300

    init 8

---
+++ censor.m
@@ -0,0 +1,10 @@
+% Introduction to Programming with MATLAB - MOOC
+% Homework 7 - Problem 8
+
+% input: strin: input cell string containing some text;
+% badword: a string word to remove from the strin
+% output: cleanstring: a cell string withou the badword
+
+function cleanstring = censor (strin, badword)
+    
+end
