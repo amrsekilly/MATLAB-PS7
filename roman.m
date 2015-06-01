d6be699910e1594da51d2ff57f738ba9423f7b05
Amr <amr.e.dev@gmail.com>
2015-06-01 23:10:21 +0300

    started thinking of 7

---
+++ roman.m
@@ -0,0 +1,24 @@
+% Introduction to Programming with MATLAB - MOOC
+% Homework 7 - Problem 7
+
+% Symbol	Value
+%    I	           1
+%   V	          5
+%   X	          10
+%
+% I placed before V or X indicates one less, so four is IV (one less than five) and nine is IX (one less than ten)
+% I placed after V or X indicates one more...
+
+% inputs: sroman - Roman symbol
+% output: arabic - Neumerical value
+function arabic = roman (sroman)
+    % get the index of I in sroman
+    % get the index of V in sroman
+    % get the index of X in sroman
+    % get count of I
+    % get count of V
+    % get count of X
+    % I is on left
+    % If ((count (V) | count(X)) & count (I) & (index(I) < index (X) | index(I) < index (V)))
+    % arabic = 
+end
