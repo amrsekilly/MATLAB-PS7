6f58e83178784908620610a7b28b1829445a7ed6
Amr <amr.e.dev@gmail.com>
2015-06-08 00:06:32 +0300

    done :3

--- censor.m
+++ censor.m
@@ -5,25 +5,15 @@
 % badword: a string word to remove from the strin
 % output: cleanstring: a cell string withou the badword
 function cleanstring = censor (strin, badword)
-   st = char(strin);
-   cleanstring = {};
-   j = 1;
-   for i = 1 : size(st, 1) % i represents each line
-   
-       % split all the words in each line 
-        splitst = strsplit(st(i, :));
-        badIndex = strcmp(splitst, badword);
-        % remove the badword 
-%         splitst(badIndex) = [];
-        if(~(sum(badIndex) > 0))
-            cleanstring(j) = cellstr(strjoin(splitst));
-            j = j + 1;
-        % push the line to cleanstri 
-        end
-        % reset badIndex
-        badIndex = 0;
-   end
-
+cleanstring = {};
+j = 1;
+for i = 1 : length(strin)
+    % find the index of badwords
+    bad_index = strfind(strin, badword);
+    if (bad_index{i})
+    else
+        cleanstring(j) = strin(i);
+        j = j + 1;
+    end
 end
-
-
+end
